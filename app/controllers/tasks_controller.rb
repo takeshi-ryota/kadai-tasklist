ae6b03744263a1cc4737a591f53f70959ce36154
takeshi.ryota <takeshi.ryota.prog@gmail.com>
2018-05-20 14:31:28 +0000

    retry

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,11 +46,13 @@ class TasksController < ApplicationController
     flash[:success] = 'タスクは正常に削除されました'
     redirect_to tasks_url
   end
-    
-end
-
- private
+  
+   private
  
  def task_params
    params.require(:task).permit(:content)
  end
+    
+end
+
+
