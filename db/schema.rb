686a56b843d980a8424d710e0296b87cfa625df3
takeshi.ryota <takeshi.ryota.prog@gmail.com>
2018-05-23 13:15:02 +0000

    kadai-tasklist

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180520104932) do
+ActiveRecord::Schema.define(version: 20180523124911) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
